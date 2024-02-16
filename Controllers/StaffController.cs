0abd92afb674c4f687b5cfb7aa768b67d64297e7
Андрей Жиянов <aazhiyanov@gmail.com>
2024-02-16 20:18:46 +0300

    Добавьте файлы проекта.

---
+++ Controllers/StaffController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStaff.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        // тестовый запрос
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+          
+            string t = $"";
+            return Ok($"Привет! Сервер запущен {DateTime.Now.ToString("D")} в {DateTime.Now.ToString("t")}");
+        }
+
+
+    }
+}
