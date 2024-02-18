9e7862d178ee0ce7c8f7db3e705d9536bf07db60
Андрей Жиянов <aazhiyanov@gmail.com>
2024-02-18 08:10:28 +0300

    add DTO

--- Models/Emploee.cs
+++ Models/Emploee.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using WebStaff.CommonModels;
 
 namespace WebStaff.Models
 {
     public class Emploee
     {
         public int Id { get; set; }
-        public string SureName { get; set; }
+        public string SurName { get; set; }
         public string Name { get; set; }
         public string? ThirdName { get; set; }
 
@@ -13,7 +14,7 @@ namespace WebStaff.Models
         public DateTime BirthDay { get; set; }
 
         [Column(TypeName = "date")] // Чтобы в базе данных был тип только date без time
-        public DateTime EmploymentDate{ get; set; }
+        public DateTime EmploymentDate { get; set; }
 
         public int Salary { get; set; }
 
@@ -22,14 +23,31 @@ namespace WebStaff.Models
         public Emploee(string sureName, string name, string thirdName,
             DateTime birfday, DateTime employmentDate, int salary)
         {
-            
-            SureName = sureName;
+
+            SurName = sureName;
             Name = name;
             ThirdName = thirdName;
             BirthDay = birfday;
             EmploymentDate = employmentDate;
             Salary = salary;
 
+        }
+
+
+        // применим патерн DTO
+        public EmploeeModel ToDto()
+        {
+            return new EmploeeModel()
+            {
+                Id = this.Id, 
+                SurName = this.SurName,
+                Name = this.Name,
+                ThirdName = this.ThirdName,
+                BirthDay = this.BirthDay,
+                EmploymentDate = this.EmploymentDate,
+                Salary = this.Salary
+
+            };
         }
 
         //- “Создание/удаление/редактирование сотрудника”
